551cc502c7e14ec3496740d791a89214a201f969
lzampieri <zampieri.leonardo98@gmail.com>
2021-09-21 13:22:58 +0200

    Add local development config

--- main/src/config.js
+++ main/src/config.js
@@ -1,7 +1,11 @@
-// For altervista upload
+// For altervista upload (npm run build)
 // export const api_url = 'https://lzampieri.altervista.org/main/cockpit/api/';
 // export const router_basename = 'main'
 
-// For local development
+// For local development (npm run start)
 export const api_url = 'http://127.0.0.1/lzampieri/main/cockpit/api/';
-export const router_basename = 'lzampieri/main'
+export const router_basename = ''
+
+// For local building (npm run build)
+// export const api_url = 'http://127.0.0.1/lzampieri/main/cockpit/api/';
+// export const router_basename = 'lzampieri/main'
