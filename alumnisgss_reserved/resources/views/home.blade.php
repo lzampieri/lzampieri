a0d6acfa63d0de18e51255b2e55facb88624ba4f
lzampieri <zampieri.leonardo98@gmail.com>
2021-12-23 22:44:19 +0100

    Created project

---
+++ alumnisgss_reserved/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <title>Laravel</title>
+
+        <livewire:styles />
+    </head>
+    <body>
+        <h1 class="text-3xl font-bold underline">
+        Hello world!
+        </h1>
+
+        <livewire:scripts />
+    </body>
+</html>
